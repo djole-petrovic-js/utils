ccb687f68b2c4ae43d90a207a8703195fa3205ae
Djole Petrovic <djordje.petrovic91@protonmail.com>
2018-02-26 22:28:07 +0100

    started LinQ functionality

---
+++ LinQ.js
@@ -0,0 +1,45 @@
+class LinQ {
+  constructor(items) {
+    this.items = items;
+  }
+
+  get() {
+    return this.items;
+  }
+
+  orderBy(fn) {
+    this.items = this.items.sort(fn);
+
+    return this;
+  }
+
+  select(fn) {
+    this.items = this.items.map(x => fn(x));
+
+    return this;
+  }
+
+  where(fn) {
+    this.items = this.items.filter(x => fn(x));
+
+    return this;
+  }
+
+  count(fn) {
+    if ( fn ) {
+      return this.where(fn).items.length;
+    } else {
+      return this.items.length;        
+    }
+  }
+
+  first() {
+    return this.items.length > 0 ? this.items[0] : null;
+  }
+
+  firstOrDefault(dft) {
+    const first = this.first();
+
+    return first ? first : dft;
+  }
+}
