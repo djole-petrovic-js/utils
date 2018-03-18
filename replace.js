ba5e88f292ad9f11b4ae80a295009819c9acc0ed
Djole Petrovic <djordje.petrovic91@protonmail.com>
2018-03-18 16:27:26 +0100

    Rewrited replace function

--- replace.js
+++ replace.js
@@ -2,26 +2,24 @@
 
   'use strict';
 
-  const replace = (str) => {
-    return (values) => {
-      const keys = Object.keys(values);
-
-      for ( const key of keys ) {
-        let regex,replaceWith;
-
-        if ( Object.prototype.toString.call(values) === '[object Array]' ) {
-          regex = /\?/g;
-          replaceWith = key;
-        } else {
-          regex = new RegExp(':' + key);
-          replaceWith = slug => values[key];
-        }
-
-        str = str.replace(regex,replaceWith);
-      }
-
-      return str;
+  const replace = (str,values) => {
+    if ( !str ) {
+      throw new Error('You have to at least provide string to be replaced...');
     }
+
+    if ( !values ) {
+      return replace.bind(null,str);
+    }
+
+    if ( Array.isArray(values) ) {
+      values.forEach(x => str = str.replace(/\?/,x));
+    } else {
+      Object.keys(values).forEach(
+        key => str = str.replace(new RegExp(':' + key),values[key])
+      );
+    }
+
+    return str;
   }
 
   window.replace = replace;
